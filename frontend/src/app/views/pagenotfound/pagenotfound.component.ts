754c640e0c0d536b8f4d08bacc284dd24b0dcf81
Gabriel Martínez <113619416+GabMtz19@users.noreply.github.com>
2023-06-21 22:29:37 -0600

    Add files via upload

---
+++ frontend/src/app/views/pagenotfound/pagenotfound.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+
+@Component({
+  selector: 'app-pagenotfound',
+  templateUrl: './pagenotfound.component.html',
+  styleUrls: ['./pagenotfound.component.scss']
+})
+export class PagenotfoundComponent implements OnInit {
+
+  constructor(private location: Location) { }
+
+  ngOnInit() {
+  }
+
+  cwcButtonClick($event) {
+    this.location.back();
+  }
+
+}
